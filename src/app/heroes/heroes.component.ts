3a156c6c0c47661af02cfc55772b98a9b048d28c
Paul Suh <paul@mspex.net>
2022-10-14 23:48:38 -0400

    Blah blah

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -15,6 +15,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   hero: Hero = {
@@ -22,7 +23,7 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm',
   };
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes();
