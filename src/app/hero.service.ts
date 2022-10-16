4eb69fa0acc51059964621936932c8455239001b
Paul Suh <paul@mspex.net>
2022-10-16 11:38:38 -0400

    Blah blah

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from "./message.service";
 
 @Injectable({
@@ -9,17 +11,22 @@ import { MessageService } from "./message.service";
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = "api/heroes";
+
+  constructor(private http: HttpClient,
+              private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private log( message: string ): void {
+    this.messageService.add( `HeroService: ${message}` );
   }
 }
