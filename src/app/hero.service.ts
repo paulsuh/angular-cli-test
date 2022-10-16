eee5ca3207994292ce3dfc710d4b72a493603a50
Paul Suh <paul@mspex.net>
2022-10-16 13:32:58 -0400

    Blah blah

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from "rxjs/operators";
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -17,7 +18,10 @@ export class HeroService {
               private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
@@ -28,5 +32,13 @@ export class HeroService {
 
   private log( message: string ): void {
     this.messageService.add( `HeroService: ${message}` );
+  }
+
+  private handleError<T>( operation = 'operation', result?: T ) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
